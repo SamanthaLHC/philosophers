9ee107ef0ede881405f2b8d54f5fff246632a986
Samantha Le Huec <s.lhuec@gmail.com>
2022-09-16 15:52:48 +0200

    makefile

---
+++ philo/srcs/philo.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sle-huec <sle-huec@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/16 15:03:51 by sle-huec          #+#    #+#             */
+/*   Updated: 2022/09/16 15:39:00 by sle-huec         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	philo(unsigned int number_of_philo, unsigned int time_to_die,
+unsigned int time_to_eat, unsigned int time_to_sleep)
+{
+
+}
+
+int	main()
+{
+	
+}
